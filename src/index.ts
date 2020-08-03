8548e57d253e70f078986c93162d6f1b33cb4964
ivanlhz <ivanlhz@gmail.com>
2020-08-03 20:25:48 +0100

    Added inquirer questions

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,57 @@
-import {getOrientation, TRobot} from './robot';
-import {isLost, calculateFinalPosition} from './utils';
+import { getOrientation, TRobot } from './robot';
+import { isLost, calculateFinalPosition } from './utils';
+import {prompt} from 'inquirer'
 
-const newMaxGridInput = '5 3';
-const robotInitInput = '1 1 E';
-const robotMovementsInput = 'RFRFRFRF';
-// const robotInitInput = '0 3 W';
-// const robotMovementsInput = 'LLFFFLFLFL';
-// const robotInitInput = '3 2 N';
-// const robotMovementsInput = 'FRRFLLFFRRFLL'
+const newRobot = async (isRepeat:boolean = false) => {
+    
+    const firstQuestion = [{
+      name: 'maxGridInput',
+      type: 'input',
+      message: 'What is the upper-right coordinates of the rectangular world?',
+    }];
 
-// Init
-const newMaxGridSplitted = newMaxGridInput.split(' ');
-const robotInitSplitted = robotInitInput.split(' ');
+    const robotQuestions = [
+      {
+        name: 'robotInitPosition',
+        message: 'What is the initial position of the robot?',
+      },
+      {
+        name: 'movements',
+        message: 'and it movements?',
+      },
+    ]
+    const questions = isRepeat ? robotQuestions : [...firstQuestion, ...robotQuestions]
 
-const robot: TRobot = calculateFinalPosition({
-  position: {x: parseInt(robotInitSplitted[0]), y: parseInt(robotInitSplitted[1])},
-  orientation: getOrientation(robotInitSplitted[2]),
-}, robotMovementsInput)
+    const answers = await prompt(questions)
+      const newMaxGridInput = answers.maxGridInput;
+      const robotInitInput = answers.robotInitPosition;
+      const robotMovementsInput = answers.movements;
+      const newMaxGridSplitted = newMaxGridInput.split(' ');
+      const robotInitSplitted = robotInitInput.split(' ');
 
-const max = {x:parseInt(newMaxGridSplitted[0]), y: parseInt(newMaxGridSplitted[1]) }
-if(isLost(robot, max)) {
-  console.log(`${robot.position.x} ${robot.position.y} ${robot.orientation} LOSS`)
-}  else {
-  console.log(`${robot.position.x} ${robot.position.y} ${robot.orientation}`)
+      const robot: TRobot = calculateFinalPosition({
+        position: { x: parseInt(robotInitSplitted[0]), y: parseInt(robotInitSplitted[1]) },
+        orientation: getOrientation(robotInitSplitted[2]),
+      }, robotMovementsInput)
+
+
+      const max = { x: parseInt(newMaxGridSplitted[0]), y: parseInt(newMaxGridSplitted[1]) }
+      if (isLost(robot, max)) {
+        console.info(`${robot.position.x} ${robot.position.y} ${robot.orientation} LOSS`)
+      } else {
+        console.info(`${robot.position.x} ${robot.position.y} ${robot.orientation}`)
+      }
+
+      const repeat = await prompt([
+        {
+          name: 'again',
+          type: 'confirm',
+          message: 'Enter another robot?',
+          default: true
+        }])
+      if (repeat.again) {
+        newRobot(true);
+      }
 }
 
+newRobot();
