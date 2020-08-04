9062f0ad08dc7a63ca45063f4968d9e770ea9822
ivanlhz <ivanlhz@gmail.com>
2020-08-04 06:10:53 +0100

    Extract questions

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,34 @@
 import { getOrientation, TRobot } from './robot';
-import { isLost, calculateFinalPosition } from './utils';
-import {prompt} from 'inquirer'
+import { isLost, calculateFinalPosition, robotQuestions, firstQuestion, endQuestion } from './utils';
+import { prompt } from 'inquirer'
 
-const newRobot = async (isRepeat:boolean = false) => {
-    
-    const firstQuestion = [{
-      name: 'maxGridInput',
-      type: 'input',
-      message: 'What is the upper-right coordinates of the rectangular world?',
-    }];
+const newRobot = async (isRepeat: boolean = false) => {
+  const questions = isRepeat ? robotQuestions : [firstQuestion, ...robotQuestions]
+  console.clear();
+  const answers = await prompt(questions)
+  const newMaxGridInput = answers.maxGridInput;
+  const robotInitInput = answers.robotInitPosition;
+  const robotMovementsInput = answers.movements;
+  const newMaxGridSplitted = newMaxGridInput.split(' ');
+  const robotInitSplitted = robotInitInput.split(' ');
 
-    const robotQuestions = [
-      {
-        name: 'robotInitPosition',
-        message: 'What is the initial position of the robot?',
-      },
-      {
-        name: 'movements',
-        message: 'and it movements?',
-      },
-    ]
-    const questions = isRepeat ? robotQuestions : [...firstQuestion, ...robotQuestions]
-
-    const answers = await prompt(questions)
-      const newMaxGridInput = answers.maxGridInput;
-      const robotInitInput = answers.robotInitPosition;
-      const robotMovementsInput = answers.movements;
-      const newMaxGridSplitted = newMaxGridInput.split(' ');
-      const robotInitSplitted = robotInitInput.split(' ');
-
-      const robot: TRobot = calculateFinalPosition({
-        position: { x: parseInt(robotInitSplitted[0]), y: parseInt(robotInitSplitted[1]) },
-        orientation: getOrientation(robotInitSplitted[2]),
-      }, robotMovementsInput)
+  const robot: TRobot = calculateFinalPosition({
+    position: { x: parseInt(robotInitSplitted[0]), y: parseInt(robotInitSplitted[1]) },
+    orientation: getOrientation(robotInitSplitted[2]),
+  }, robotMovementsInput)
 
 
-      const max = { x: parseInt(newMaxGridSplitted[0]), y: parseInt(newMaxGridSplitted[1]) }
-      if (isLost(robot, max)) {
-        console.info(`${robot.position.x} ${robot.position.y} ${robot.orientation} LOSS`)
-      } else {
-        console.info(`${robot.position.x} ${robot.position.y} ${robot.orientation}`)
-      }
+  const max = { x: parseInt(newMaxGridSplitted[0]), y: parseInt(newMaxGridSplitted[1]) }
+  if (isLost(robot, max)) {
+    console.info(`${robot.position.x} ${robot.position.y} ${robot.orientation} LOSS`)
+  } else {
+    console.info(`${robot.position.x} ${robot.position.y} ${robot.orientation}`)
+  }
 
-      const repeat = await prompt([
-        {
-          name: 'again',
-          type: 'confirm',
-          message: 'Enter another robot?',
-          default: true
-        }])
-      if (repeat.again) {
-        newRobot(true);
-      }
+  const repeat = await prompt([endQuestion])
+  if (repeat.again) {
+    newRobot(true);
+  }
 }
 
 newRobot();
--- src/utils.ts
+++ src/utils.ts
@@ -17,3 +17,28 @@ export const calculateFinalPosition = (robot: TRobot, movements: string ) => {
   }
   return _robot
 }
+
+export const firstQuestion = {
+  name: 'maxGridInput',
+  type: 'input',
+  message: 'What is the upper-right coordinates of the rectangular world?',
+};
+
+export const robotQuestions = [
+  {
+    name: 'robotInitPosition',
+    message: 'What is the initial position of the robot?',
+  },
+  {
+    name: 'movements',
+    message: 'and it movements?',
+  },
+]
+
+export const endQuestion = 
+  {
+    name: 'again',
+    type: 'confirm',
+    message: 'Enter another robot?',
+    default: true
+  };
